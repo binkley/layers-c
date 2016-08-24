99422b585c243aecf0bccc2721698381d84b1783
Brian Oxley <boxley@thoughtworks.com>
2016-08-24 11:53:17 -0500

    Line endings are nice

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 #include "main.h"
 
 int main() {
-    printf("Hello, world, from main() version %d!", Demo_VERSION_MAJOR);
+    printf("Hello, world, from main() version %d!\n", Demo_VERSION_MAJOR);
     return 0;
 }
