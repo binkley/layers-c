1165fd104f454c790c20eba3b7537c7f095d2c1b
Brian Oxley <boxley@thoughtworks.com>
2016-08-25 09:12:44 -0500

    Actually use GC

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 
 int
 main(int argc, char **argv) {
+    GC_INIT();
     printf("Hello, world, from [demo:%s:%d:%s] version %d!\n",
            __FILE__, __LINE__, __PRETTY_FUNCTION__, Demo_VERSION_MAJOR);
+    GC_MALLOC(2 * 1024);
     return 0;
 }
