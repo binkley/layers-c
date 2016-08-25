6a8d0e5effba4af492d810706eec076e5daacade
Brian Oxley <boxley@thoughtworks.com>
2016-08-25 09:01:05 -0500

    Add BDW-GC package

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#include <gc.h>
+
 #include "main.h"
 
-int main() {
-    printf("Hello, world, from main() version %d!\n", Demo_VERSION_MAJOR);
+int
+main(int argc, char **argv) {
+    printf("Hello, world, from [demo:%s:%d:%s] version %d!\n",
+           __FILE__, __LINE__, __PRETTY_FUNCTION__, Demo_VERSION_MAJOR);
     return 0;
 }
